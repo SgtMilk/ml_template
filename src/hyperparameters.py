0a562ace5023b428258b1989f29193b6b811884e
SgtMilk <alix.routhier-lalonde@hotmail.com>
2022-06-28 22:53:46 -0400

    initial setup

---
+++ src/hyperparameters.py
@@ -0,0 +1,26 @@
+from torch.nn import MSELoss, BCELoss
+from torch.optim import Adam
+import torch
+
+# from src.model import RNN
+
+
+class Params:
+    # device = torch.device("cpu")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    no_download = True
+
+    look_back = 100
+
+    # model parameters
+    # model = RNN
+    hidden_dim = 256
+    num_dim = 4
+    dropout = 0.4
+    validation_split = 0.1
+
+    # training parameters
+    epochs = 150
+    learning_rate = 0.0001
+    loss = MSELoss(reduction="mean")
+    optimizer = Adam
---
+++ train.py
@@ -0,0 +1,6 @@
+def train():
+    print("train")
+
+
+if __name__ == "__main__":
+    train()
